075836e1a99749be1686a6d5be69974571b42ce8
powerHouse <139514412+ahmedyoussef-b@users.noreply.github.com>
2026-08-14 13:52:40 +0100

    Suppression: src/app/pipeline/page.tsx

--- src/app/pipeline/page.tsx
+++
@@ -1,9 +0,0 @@
-﻿import { DeployPipeline } from '@/components/pipeline/DeployPipeline';
-
-export default function PipelinePage() {
-  return (
-    <div className="container mx-auto p-6 max-w-4xl">
-      <DeployPipeline />
-    </div>
-  );
-}
